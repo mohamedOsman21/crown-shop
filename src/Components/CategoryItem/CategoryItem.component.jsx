763ac40e2565997d415ac305fa15d2c5bd9712c6
unknown <mohamedosman2132001@gmail.com>
2025-07-22 02:16:21 +0300

    the first stage of the project

---
+++ src/Components/CategoryItem/CategoryItem.component.jsx
@@ -0,0 +1,18 @@
+import './CategoryItem.styles.scss';
+
+function CategoryItem ({category}) {
+    const {title, imageUrl} = category;
+    return (
+        <div className='category-container' >
+            <div className="background-image"  style={{
+                backgroundImage: `url(${imageUrl})`
+            }}></div>
+            <div className="category-body-container">
+                <h2>{title}</h2>
+                <p>shop now</p>
+            </div>
+        </div>
+    );
+}
+
+export default CategoryItem;
